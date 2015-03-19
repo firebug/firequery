a4c71ba17d1cd7f2c1f00d1def5714107e21a97d
Jan Odvarko <odvarko@gmail.com>
2015-03-19 17:30:00 +0100

    #6 content <-> chrome messaging

--- data/inspector.js
+++ data/inspector.js
@@ -5,5 +5,22 @@ define(function(require, exports, module) {
 // Dependencies
 const React = require("react");
 
+/**
+ * Post message to the chrome through DOM event dispatcher.
+ * (there is no message manager for the markupview.xhtml frame).
+ */
+function postChromeMessage(type, data) {
+  const event = new MessageEvent("firequery/message", {
+    bubbles: true,
+    cancelable: true,
+    type: type,
+    data: data,
+  });
+
+  dispatchEvent(event);
+}
+
+postChromeMessage("test", "a message from the content");
+
 // End of inspector.js
 });
