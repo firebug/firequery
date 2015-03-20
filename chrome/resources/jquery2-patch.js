9a00bf8a88dca99e733e408e11458ee4cb69fe80
Jan Odvarko <odvarko@gmail.com>
2015-03-20 19:25:06 +0100

    #3 jQuery patch code style fixes

--- chrome/resources/jquery2-patch.js
+++ chrome/resources/jquery2-patch.js
@@ -1,6 +1,10 @@
-// this code wraps jQuery's data-related methods
-// wrapped methods send events to firequery when data is manipulated on element
-// this code is compatible with jQuery 2.0+
+/* See license.txt for terms of usage */
+
+/**
+ * This code wraps jQuery's data-related methods
+ * wrapped methods send events to FireQuery when data is manipulated
+ * on element. This code is compatible with jQuery 2.0+
+ */
 (function($) {
 
   // wrap data calls on collections
@@ -11,18 +15,18 @@
   var wrap = function(originalImp) {
     return function() {
       // snapshot previous state, apply original implementation, snapshot new state
-      var oldValues = $.extend(true, {}, originalDataFn.apply(this)); // need to do a deep copy of the whole structure
+      //var oldValues = $.extend(true, {}, originalDataFn.apply(this)); // need to do a deep copy of the whole structure
       var res = originalImp.apply(this, arguments);
-      var newValues = $.extend(true, {}, originalDataFn.apply(this)); // need to do a deep copy of the whole structure
+      //var newValues = $.extend(true, {}, originalDataFn.apply(this)); // need to do a deep copy of the whole structure
       // prepare event payload
-      var detail = {
+      /*var detail = {
         bubbles: true,
         oldValues: oldValues,
         newValues: newValues
-      };
+      };*/
       // send event for each matched element
       this.each(function() {
-        var event = new CustomEvent("firequery-event", { "detail": detail });
+        var event = new CustomEvent("firequery-event"/*, { "detail": detail }*/);
         this.dispatchEvent(event);
       });
       // simulate original return value
@@ -41,17 +45,17 @@
   var wrap2 = function(originalImp) {
     return function(elem) {
       // snapshot previous state, apply original implementation, snapshot new state
-      var oldValues = $.extend(true, {}, originalData.apply(this, [elem])); // need to do a deep copy of the whole structure
+      //var oldValues = $.extend(true, {}, originalData.apply(this, [elem])); // need to do a deep copy of the whole structure
       var res = originalImp.apply(this, arguments);
-      var newValues = $.extend(true, {}, originalData.apply(this, [elem])); // need to do a deep copy of the whole structure
+      //var newValues = $.extend(true, {}, originalData.apply(this, [elem])); // need to do a deep copy of the whole structure
       // prepare event payload
-      var detail = {
+      /*var detail = {
         bubbles: true,
         oldValues: oldValues,
         newValues: newValues
-      };
+      };*/
       // send event
-      var event = new CustomEvent("firequery-event", { "detail": detail });
+      var event = new CustomEvent("firequery-event"/*, { "detail": detail }*/);
       elem.dispatchEvent(event);
       // simulate original return value
       return res;
